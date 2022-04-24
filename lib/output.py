eb816b6dbf904f359bba8b050954155eca27e514
45qq <2192003656@qq.com>
2022-04-24 13:07:16 +0800

    导出 URL 时去重。

--- lib/output.py
+++ lib/output.py
@@ -22,8 +22,9 @@ def output_file(mode, q, data: list):
 
     data.sort(key=lambda item: item[0][0])
 
-    with open(os.path.join(output_dir, "q.txt"), 'w', encoding='utf-8') as f:
-        f.write(q)
+    if not os.path.exists(os.path.join(output_dir, "q.txt")):
+        with open(os.path.join(output_dir, "q.txt"), 'w', encoding='utf-8') as f:
+            f.write(q)
 
     if mode == 0:
         path = os.path.join(output_dir, "data.csv")
@@ -38,13 +39,15 @@ def output_file(mode, q, data: list):
         path = os.path.join(output_dir, "data_url.txt")
         if os.path.exists(path):
             return 0
+        url_list = []
         index = get_head_index("URL")
         with open(path, 'w', encoding='utf-8') as f:
             for group in data:
                 for row in group:
-                    if row[index] != '':
+                    if row[index] != '' and row[index] not in url_list:
                         f.write(row[index])
                         f.write('\n')
+                        url_list.append(row[index])
     elif mode == 2:
         path = os.path.join(output_dir, "data_ip.txt")
         if os.path.exists(path):
